949b55d3d14ac6f735db036b87ac8637be3b80b1
Dallon Feldner <dallon.feldner@gmail.com>
2020-06-30 16:54:18 -0500

    Day 2 Part Two

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,4 +1,4 @@
-// Day NN: Description
+// Day 2: 1202 Program Alarm
 
 use crate::prelude::*;
 
@@ -14,51 +14,51 @@ pub fn parse(input: &str) -> IntcodeSequence {
   input.split(",").map(|num| num.parse().unwrap()).collect()
 }
 
-fn compute_instruction(sequence: &mut IntcodeSequence, position: usize) -> ProgramState {
-  let instruction = sequence[position];
+fn compute_instruction(sequence: &mut IntcodeSequence, instruction_pointer: usize) -> ProgramState {
+  let instruction = sequence[instruction_pointer];
   match instruction {
     1 => {
       // Add
-      let a_addr = sequence[position + 1];
-      let b_addr = sequence[position + 2];
-      let result_addr = sequence[position + 3];
+      let a_addr = sequence[instruction_pointer + 1];
+      let b_addr = sequence[instruction_pointer + 2];
+      let result_addr = sequence[instruction_pointer + 3];
 
       let a = sequence[a_addr];
       let b = sequence[b_addr];
       let result = &mut sequence[result_addr];
       *result = a + b;
 
-      ProgramState::Continue(position + 4)
+      ProgramState::Continue(instruction_pointer + 4)
     }
     2 => {
       // Multiply
-      let a_addr = sequence[position + 1];
-      let b_addr = sequence[position + 2];
-      let result_addr = sequence[position + 3];
+      let a_addr = sequence[instruction_pointer + 1];
+      let b_addr = sequence[instruction_pointer + 2];
+      let result_addr = sequence[instruction_pointer + 3];
 
       let a = sequence[a_addr];
       let b = sequence[b_addr];
       let result = &mut sequence[result_addr];
       *result = a * b;
 
-      ProgramState::Continue(position + 4)
+      ProgramState::Continue(instruction_pointer + 4)
     }
     99 => ProgramState::Halt,
     _ => {
       panic!(format!(
-        "Unrecognized instruction {} at position {}",
-        instruction, position
+        "Unrecognized instruction {} at instruction pointer {}",
+        instruction, instruction_pointer
       ));
     }
   }
 }
 
 pub fn compute(sequence: &mut IntcodeSequence) -> usize {
-  let mut position = 0;
+  let mut instruction_pointer = 0;
   loop {
-    let result = compute_instruction(sequence, position);
+    let result = compute_instruction(sequence, instruction_pointer);
     match result {
-      ProgramState::Continue(new_position) => position = new_position,
+      ProgramState::Continue(new_position) => instruction_pointer = new_position,
       ProgramState::Halt => return sequence[0],
     }
   }
@@ -67,6 +67,27 @@ pub fn compute(sequence: &mut IntcodeSequence) -> usize {
 pub fn parse_and_compute(input: &str) -> usize {
   let mut sequence = parse(input);
   compute(&mut sequence)
+}
+
+pub fn brute_force_answer(
+  sequence: &IntcodeSequence,
+  noun_addr: usize,
+  verb_addr: usize,
+  desired_output: usize,
+) -> (usize, usize) {
+  for noun_candidate in 0..100 {
+    for verb_candidate in 0..100 {
+      let mut candidate_sequence = sequence.clone();
+      candidate_sequence[noun_addr] = noun_candidate;
+      candidate_sequence[verb_addr] = verb_candidate;
+      let result = compute(&mut candidate_sequence);
+      if result == desired_output {
+        return (noun_candidate, verb_candidate);
+      }
+    }
+  }
+
+  panic!("No answer found");
 }
 
 lazy_static! {
@@ -107,11 +128,14 @@ mod part_one {
   }
 }
 
-// #[cfg(test)]
-// mod part_two {
-//   use super::*;
-//   #[test]
-//   fn test_cases() {}
-//   #[test]
-//   fn part_two() {}
-// }
+#[cfg(test)]
+mod part_two {
+  use super::*;
+  #[test]
+  fn part_two() {
+    let sequence = parse(&PUZZLE_INPUT);
+    let (noun, verb) = brute_force_answer(&sequence, 1, 2, 19690720);
+    let result = 100 * noun + verb;
+    assert_eq!(result, 4925);
+  }
+}
