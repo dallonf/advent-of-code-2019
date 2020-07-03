8d454d0c4414d4d038334d168406bc53653035cb
Dallon Feldner <dallon.feldner@gmail.com>
2020-07-03 15:43:39 -0500

    Day 3 Part Two

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -2,11 +2,15 @@
 
 use crate::prelude::*;
 use core::ops::{Add, Neg, Sub};
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct Point(i32, i32);
-pub type Wire = HashSet<Point>;
+pub struct WirePointData {
+  steps_to_point: i32,
+}
+pub type Wire = HashMap<Point, WirePointData>;
 
 impl Point {
   fn manhattan_distance_from_center(&self) -> i32 {
@@ -37,7 +41,7 @@ impl Sub for Point {
 
 pub fn parse_wire(input: &str) -> Wire {
   let mut current_location = Point(0, 0);
-  let mut result = HashSet::new();
+  let mut result: Wire = HashMap::new();
 
   let turns = input.split(",");
 
@@ -54,7 +58,12 @@ pub fn parse_wire(input: &str) -> Wire {
 
     for _ in 0..num {
       current_location = move_in_direction(current_location);
-      result.insert(current_location);
+      result.insert(
+        current_location,
+        WirePointData {
+          steps_to_point: result.len() as i32 + 1,
+        },
+      );
     }
   }
 
@@ -62,18 +71,44 @@ pub fn parse_wire(input: &str) -> Wire {
 }
 
 pub fn get_closest_cross<'a>(wire1: &'a Wire, wire2: &'a Wire) -> Option<&'a Point> {
-  let cross_points = wire1.intersection(&wire2);
-  cross_points.min_by(|point1, point2| {
-    point1
-      .manhattan_distance_from_center()
-      .cmp(&point2.manhattan_distance_from_center())
-  })
+  let wire1_points: HashSet<_> = wire1.keys().collect();
+  let wire2_points: HashSet<_> = wire2.keys().collect();
+  let cross_points: Vec<&Point> = wire1_points.intersection(&wire2_points).cloned().collect();
+
+  cross_points
+    .iter()
+    .min_by(|point1, point2| {
+      point1
+        .manhattan_distance_from_center()
+        .cmp(&point2.manhattan_distance_from_center())
+    })
+    .cloned()
 }
 
 pub fn get_closest_cross_distance_from_wire_strings(wire1: &str, wire2: &str) -> Option<i32> {
   let wire1 = parse_wire(wire1);
   let wire2 = parse_wire(wire2);
   get_closest_cross(&wire1, &wire2).map(|x| x.manhattan_distance_from_center())
+}
+
+pub fn get_shortest_cross_time(wire1: &Wire, wire2: &Wire) -> Option<i32> {
+  let wire1_points: HashSet<_> = wire1.keys().collect();
+  let wire2_points: HashSet<_> = wire2.keys().collect();
+  let cross_points: Vec<&Point> = wire1_points.intersection(&wire2_points).cloned().collect();
+
+  cross_points
+    .iter()
+    .map(|point| {
+      let total_steps = &wire1[point].steps_to_point + &wire2[point].steps_to_point;
+      total_steps
+    })
+    .min()
+}
+
+pub fn get_shortest_cross_time_from_wire_strings(wire1: &str, wire2: &str) -> Option<i32> {
+  let wire1 = parse_wire(wire1);
+  let wire2 = parse_wire(wire2);
+  get_shortest_cross_time(&wire1, &wire2)
 }
 
 lazy_static! {
@@ -122,11 +157,41 @@ mod part_one {
   }
 }
 
-// #[cfg(test)]
-// mod part_two {
-//   use super::*;
-//   #[test]
-//   fn test_cases() {}
-//   #[test]
-//   fn part_two() {}
-// }
+#[cfg(test)]
+mod part_two {
+  use super::*;
+
+  #[test]
+  fn example() {
+    let wire1 = parse_wire("R8,U5,L5,D3");
+    let wire2 = parse_wire("U7,R6,D4,L4");
+    let closest_cross = get_shortest_cross_time(&wire1, &wire2);
+    assert_eq!(closest_cross, Some(30));
+  }
+
+  #[test]
+  fn test_cases() {
+    assert_eq!(
+      get_shortest_cross_time_from_wire_strings(
+        "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+        "U62,R66,U55,R34,D71,R55,D58,R83"
+      ),
+      Some(610)
+    );
+    assert_eq!(
+      get_shortest_cross_time_from_wire_strings(
+        "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+        "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+      ),
+      Some(410)
+    );
+  }
+
+  #[test]
+  fn part_two() {
+    assert_eq!(
+      get_shortest_cross_time_from_wire_strings(&PUZZLE_INPUT[0], &PUZZLE_INPUT[1]),
+      Some(12934)
+    )
+  }
+}
