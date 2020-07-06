16864f3e3b3c6306c84f7cd40c8a3c540e935b6e
Dallon Feldner <dallon.feldner@gmail.com>
2020-07-06 13:21:43 -0500

    Refactor Intcode computer into its own module

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,76 +1,10 @@
 // Day 2: 1202 Program Alarm
 
+use crate::logic::intcode;
 use crate::prelude::*;
 
-pub type IntcodeSequence = Vec<usize>;
-
-#[derive(Debug, PartialEq)]
-enum ProgramState {
-  Continue(usize),
-  Halt,
-}
-
-pub fn parse(input: &str) -> IntcodeSequence {
-  input.split(",").map(|num| num.parse().unwrap()).collect()
-}
-
-fn compute_instruction(sequence: &mut IntcodeSequence, instruction_pointer: usize) -> ProgramState {
-  let instruction = sequence[instruction_pointer];
-  match instruction {
-    1 => {
-      // Add
-      let a_addr = sequence[instruction_pointer + 1];
-      let b_addr = sequence[instruction_pointer + 2];
-      let result_addr = sequence[instruction_pointer + 3];
-
-      let a = sequence[a_addr];
-      let b = sequence[b_addr];
-      let result = &mut sequence[result_addr];
-      *result = a + b;
-
-      ProgramState::Continue(instruction_pointer + 4)
-    }
-    2 => {
-      // Multiply
-      let a_addr = sequence[instruction_pointer + 1];
-      let b_addr = sequence[instruction_pointer + 2];
-      let result_addr = sequence[instruction_pointer + 3];
-
-      let a = sequence[a_addr];
-      let b = sequence[b_addr];
-      let result = &mut sequence[result_addr];
-      *result = a * b;
-
-      ProgramState::Continue(instruction_pointer + 4)
-    }
-    99 => ProgramState::Halt,
-    _ => {
-      panic!(format!(
-        "Unrecognized instruction {} at instruction pointer {}",
-        instruction, instruction_pointer
-      ));
-    }
-  }
-}
-
-pub fn compute(sequence: &mut IntcodeSequence) -> usize {
-  let mut instruction_pointer = 0;
-  loop {
-    let result = compute_instruction(sequence, instruction_pointer);
-    match result {
-      ProgramState::Continue(new_position) => instruction_pointer = new_position,
-      ProgramState::Halt => return sequence[0],
-    }
-  }
-}
-
-pub fn parse_and_compute(input: &str) -> usize {
-  let mut sequence = parse(input);
-  compute(&mut sequence)
-}
-
 pub fn brute_force_answer(
-  sequence: &IntcodeSequence,
+  sequence: &intcode::IntcodeSequence,
   noun_addr: usize,
   verb_addr: usize,
   desired_output: usize,
@@ -85,7 +19,7 @@ pub fn brute_force_answer(
       let mut candidate_sequence = sequence.clone();
       candidate_sequence[noun_addr] = noun_candidate;
       candidate_sequence[verb_addr] = verb_candidate;
-      let result = compute(&mut candidate_sequence);
+      let result = intcode::compute(&mut candidate_sequence);
       result == desired_output
     })
     .expect("No answer found")
@@ -102,30 +36,30 @@ mod part_one {
   #[test]
   fn run_single_instruction() {
     let program = "1,9,10,3,2,3,11,0,99,30,40,50";
-    let mut sequence = parse(program);
-    let result = compute_instruction(&mut sequence, 0);
-    assert_eq!(result, ProgramState::Continue(4));
+    let mut sequence = intcode::parse(program);
+    let result = intcode::compute_instruction(&mut sequence, 0);
+    assert_eq!(result, intcode::ProgramState::Continue(4));
     assert_eq!(sequence[3], 70);
   }
 
   #[test]
   fn detailed_test_case() {
     let program = "1,9,10,3,2,3,11,0,99,30,40,50";
-    assert_eq!(parse_and_compute(program), 3500);
+    assert_eq!(intcode::parse_and_compute(program), 3500);
   }
 
   #[test]
   fn test_cases() {
-    assert_eq!(parse_and_compute("1,0,0,0,99"), 2);
-    assert_eq!(parse_and_compute("1,1,1,4,99,5,6,0,99"), 30);
+    assert_eq!(intcode::parse_and_compute("1,0,0,0,99"), 2);
+    assert_eq!(intcode::parse_and_compute("1,1,1,4,99,5,6,0,99"), 30);
   }
 
   #[test]
   fn answer() {
-    let mut sequence = parse(&PUZZLE_INPUT);
+    let mut sequence = intcode::parse(&PUZZLE_INPUT);
     sequence[1] = 12;
     sequence[2] = 2;
-    assert_eq!(compute(&mut sequence), 5305097);
+    assert_eq!(intcode::compute(&mut sequence), 5305097);
   }
 }
 
@@ -136,7 +70,7 @@ mod part_two {
   #[test]
   fn part_two() {
     let start = time::Instant::now();
-    let sequence = parse(&PUZZLE_INPUT);
+    let sequence = intcode::parse(&PUZZLE_INPUT);
     let (noun, verb) = brute_force_answer(&sequence, 1, 2, 19690720);
     let result = 100 * noun + verb;
     assert_eq!(result, 4925);
---
+++ src/logic/intcode.rs
@@ -0,0 +1,66 @@
+pub type IntcodeSequence = Vec<usize>;
+
+#[derive(Debug, PartialEq)]
+pub enum ProgramState {
+  Continue(usize),
+  Halt,
+}
+
+pub fn parse(input: &str) -> IntcodeSequence {
+  input.split(",").map(|num| num.parse().unwrap()).collect()
+}
+
+pub fn compute_instruction(sequence: &mut IntcodeSequence, instruction_pointer: usize) -> ProgramState {
+  let instruction = sequence[instruction_pointer];
+  match instruction {
+    1 => {
+      // Add
+      let a_addr = sequence[instruction_pointer + 1];
+      let b_addr = sequence[instruction_pointer + 2];
+      let result_addr = sequence[instruction_pointer + 3];
+
+      let a = sequence[a_addr];
+      let b = sequence[b_addr];
+      let result = &mut sequence[result_addr];
+      *result = a + b;
+
+      ProgramState::Continue(instruction_pointer + 4)
+    }
+    2 => {
+      // Multiply
+      let a_addr = sequence[instruction_pointer + 1];
+      let b_addr = sequence[instruction_pointer + 2];
+      let result_addr = sequence[instruction_pointer + 3];
+
+      let a = sequence[a_addr];
+      let b = sequence[b_addr];
+      let result = &mut sequence[result_addr];
+      *result = a * b;
+
+      ProgramState::Continue(instruction_pointer + 4)
+    }
+    99 => ProgramState::Halt,
+    _ => {
+      panic!(format!(
+        "Unrecognized instruction {} at instruction pointer {}",
+        instruction, instruction_pointer
+      ));
+    }
+  }
+}
+
+pub fn compute(sequence: &mut IntcodeSequence) -> usize {
+  let mut instruction_pointer = 0;
+  loop {
+    let result = compute_instruction(sequence, instruction_pointer);
+    match result {
+      ProgramState::Continue(new_position) => instruction_pointer = new_position,
+      ProgramState::Halt => return sequence[0],
+    }
+  }
+}
+
+pub fn parse_and_compute(input: &str) -> usize {
+  let mut sequence = parse(input);
+  compute(&mut sequence)
+}
