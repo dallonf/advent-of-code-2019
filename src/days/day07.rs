76306d96de6552eed96806e67044811ba5801be4
Dallon Feldner <dallon.feldner@gmail.com>
2020-07-14 12:22:30 -0500

    More test cases

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -7,7 +7,7 @@ use crate::prelude::*;
 pub type PhaseSettingSequence = [u8; 5];
 
 pub fn compute_thruster_signal(
-  sequence: intcode::IntcodeSequence,
+  sequence: &intcode::IntcodeSequence,
   phase_settings: &PhaseSettingSequence,
 ) -> isize {
   let mut signal = 0;
@@ -51,10 +51,28 @@ mod part_one {
   fn test_cases() {
     assert_eq!(
       compute_thruster_signal(
-        intcode::IntcodeSequence::parse("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"),
+        &intcode::IntcodeSequence::parse("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"),
         &[4, 3, 2, 1, 0],
       ),
       43210
+    );
+    assert_eq!(
+      compute_thruster_signal(
+        &intcode::IntcodeSequence::parse(
+          "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
+        ),
+        &[0, 1, 2, 3, 4],
+      ),
+      54321
+    );
+    assert_eq!(
+      compute_thruster_signal(
+        &intcode::IntcodeSequence::parse(
+          "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
+        ),
+        &[1, 0, 4, 3, 2],
+      ),
+      65210
     );
   }
   // #[test]
